3b5a3d3d0388b81508259464b3a4f7478f784993
andang.son <andang.son@gmail.com>
2012-01-06 07:04:00 +0000

    spring 3.1.0.RELEASE 버전으로 업그레이드

---
+++ architecture-web-ui/src/main/java/architecture/ee/web/struts2/action/setup/MenuAction.java
@@ -0,0 +1,65 @@
+package architecture.ee.web.struts2.action.setup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import architecture.ee.util.OutputFormat;
+import architecture.ee.web.struts2.action.FrameworkActionSupport;
+import architecture.ee.web.ui.menu.Menu;
+import architecture.ee.web.ui.menu.MenuBuilder;
+
+import com.opensymphony.xwork2.Preparable;
+
+public class MenuAction  extends FrameworkActionSupport implements Preparable {
+
+	public static final String VIEW_MENUS = "menus";
+	
+	private MenuBuilder menuBuilder;	
+	
+	public MenuBuilder getMenuBuilder() {
+		return menuBuilder;
+	}
+
+	public void setMenuBuilder(MenuBuilder menuBuilder) {
+		this.menuBuilder = menuBuilder;
+	}
+
+	public void prepare() throws Exception {
+		models.clear();
+	}
+
+	public List<Menu> getAllMenus(){
+		return menuBuilder.getAllMenus();
+	}
+	
+	public String execute()
+    {
+    	models.clear();
+    	if(VIEW_MENUS.equals( getView() )){
+    		if( OutputFormat.stingToOutputFormat( getDataType() ) == OutputFormat.JSON ){ 	
+    			
+    			List<Menu> menus = getAllMenus();
+    			List<Map> list = new ArrayList<Map>(menus.size());    
+    			for( Menu menu : menus){
+    				Map<String, Object> row = new HashMap<String, Object>();
+    				row.put("ID", menu.getMenuId());
+    				row.put("name", menu.getName());
+    				row.put("title", menu.getTitle());    				
+    				row.put("location", menu.getLocation());
+    				row.put("enabled", menu.isEnabled());
+    				row.put("items", menu.getMenuItems());
+    				list.add(row);
+    			}    			
+    			models.put("menus", list );    			
+    			return JSON;
+    		}else if ( OutputFormat.stingToOutputFormat( getDataType() ) == OutputFormat.XML ){     
+    			models.put("menus", getAllMenus());
+    			return XML;
+    		}
+    	}    	
+        return SUCCESS;
+    }
+	
+}
