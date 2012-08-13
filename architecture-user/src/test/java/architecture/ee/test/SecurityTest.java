6c50ff2fe9b0bdbd54513bdf2b1a37620329df7f
andang.son <andang.son@gmail.com>
2012-08-13 07:28:57 +0000

    아키텍처 업데이트

--- architecture-user/src/test/java/architecture/ee/test/SecurityTest.java
+++ architecture-user/src/test/java/architecture/ee/test/SecurityTest.java
@@ -6,7 +6,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
-import architecture.user.security.permission.Permissions;
+import architecture.security.permission.Permissions;
 
 public class SecurityTest {
 
