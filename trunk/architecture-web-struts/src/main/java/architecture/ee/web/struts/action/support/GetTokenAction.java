3e928c8d9f508b835eb36322cc754c3f27a39133
andang.son@gmail.com <andang.son@gmail.com@2089a4da-beaa-ddd3-21c0-bc11941dc8e0>
2012-10-11 01:56:34 +0000

    스트럿츠 Action 및 사용자 모듈관련 Action 수정

---
+++ trunk/architecture-web-struts/src/main/java/architecture/ee/web/struts/action/support/GetTokenAction.java
@@ -0,0 +1,46 @@
+package architecture.ee.web.struts.action.support;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import architecture.ee.util.OutputFormat;
+import architecture.ee.web.struts.action.FrameworkActionSupport;
+import architecture.ee.web.view.json.JsonView;
+
+public class GetTokenAction extends FrameworkActionSupport  {
+	@Override
+	public ActionForward execute(
+			ActionMapping mapping, 
+			ActionForm form,
+			HttpServletRequest request, 
+			HttpServletResponse response)
+			throws Exception {
+		
+		OutputFormat output = getOutputFormat(request, response);		
+		Map model = getModelMap(request, response);
+		Map<String, String> item = new java.util.HashMap<String, String>() ; 
+		
+		String token = (String)request.getSession().getAttribute(org.apache.struts.Globals.TRANSACTION_TOKEN_KEY);				
+		
+		item.put("success", "true");		
+		item.put("token", token );		
+		model.put("item", item );		
+		saveModelMap(request, model);		
+		
+		if(output == OutputFormat.JSON ){
+			JsonView view = new JsonView();		    
+			view.setModelKey("item");			
+			view.render(model, request, response);
+			return null;
+		}
+		
+		return (mapping.findForward(output.name().toLowerCase()));
+		
+	}
+}
