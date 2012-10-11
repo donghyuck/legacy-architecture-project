29d147b3a40e6fc48293318097d093182ef36035
andang.son <andang.son@gmail.com>
2012-10-11 01:40:50 +0000

    사용자 모듈 수정

---
+++ architecture-user/src/main/java/architecture/security/spring/authentication/encoding/PasswordEncoderFactory.java
@@ -0,0 +1,60 @@
+package architecture.security.spring.authentication.encoding;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+
+import architecture.common.lifecycle.ApplicationProperties;
+import architecture.ee.component.admin.AdminHelper;
+
+/**
+ * @author  donghyuck
+ */
+public class PasswordEncoderFactory implements FactoryBean <PasswordEncoder> {
+
+	public static final String DEFAUTL_ENCODING_ALGORITHM = "SHA-1";
+	
+	/**
+	 * @uml.property  name="singleton"
+	 */
+	private boolean singleton = true ;
+	
+	private PasswordEncoder passwordEncoder = null;
+	
+	public PasswordEncoder getObject() throws Exception {
+		if( passwordEncoder == null){
+			//SHA 1, 256, 384, 512, MD4, MD5	
+			ApplicationProperties setupProperties = AdminHelper.getRepository().getSetupApplicationProperties();
+			String algorithm = setupProperties.get("security.authentication.encoding.algorithm");
+			if(StringUtils.isEmpty(algorithm))
+				algorithm = DEFAUTL_ENCODING_ALGORITHM ;			
+			MessageDigestPasswordEncoder encoder = new MessageDigestPasswordEncoder(algorithm);
+			this.passwordEncoder = encoder;
+		}
+		return passwordEncoder;
+	}
+
+	public Class<PasswordEncoder> getObjectType() {
+		return PasswordEncoder.class;
+	}
+
+	/**
+	 * @param singleton
+	 * @uml.property  name="singleton"
+	 */
+	public void setSingleton(boolean singleton) {
+		this.singleton = singleton;
+	}
+	
+	/**
+	 * @return
+	 * @uml.property  name="singleton"
+	 */
+	public boolean isSingleton() {
+		return this.singleton;
+	}
+
+	
+	
+}
