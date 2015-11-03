03162c0d58d4c43b985977d66ec7c9b64f7f9337
donghyuck <andang.son@gmail.com>
2015-11-03 11:20:47 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

--- architecture-community/src/test/java/architecture/ee/CrawlerTest.java
+++ architecture-community/src/test/java/architecture/ee/CrawlerTest.java
@@ -25,8 +25,65 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
-public class CrawlerTest {
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+public class CrawlerTest {
+	@Test
+	public void crawlling() throws Exception {
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder("/Users/donghyuck/Documents/temp");
+		config.setPolitenessDelay(1000);
+
+	    /*
+	     * You can set the maximum crawl depth here. The default value is -1 for
+	     * unlimited depth
+	     */
+	    config.setMaxDepthOfCrawling(2);
+
+	    /*
+	     * You can set the maximum number of pages to crawl. The default value
+	     * is -1 for unlimited number of pages
+	     */
+	    config.setMaxPagesToFetch(1000);
+
+	    /**
+	     * Do you want crawler4j to crawl also binary data ?
+	     * example: the contents of pdf, or the metadata of images etc
+	     */
+	    config.setIncludeBinaryContentInCrawling(false);
+
+	    /*
+	     * Do you need to set a proxy? If so, you can use:
+	     * config.setProxyHost("proxyserver.example.com");
+	     * config.setProxyPort(8080);
+	     *
+	     * If your proxy also needs authentication:
+	     * config.setProxyUsername(username); config.getProxyPassword(password);
+	     */
+
+	    /*
+	     * This config parameter can be used to set your crawl to be resumable
+	     * (meaning that you can resume the crawl from a previously
+	     * interrupted/crashed crawl). Note: if you enable resuming feature and
+	     * want to start a fresh crawl, you need to delete the contents of
+	     * rootFolder manually.
+	     */
+	    config.setResumableCrawling(false);
+	    
+	    PageFetcher pageFetcher = new PageFetcher(config);
+	    
+	    RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+	    RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+	    
+	    CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+	    controller.addSeed("http://www.naver.com/");
+	    controller.start(MyCrawler.class, 1);
+		
+	}
 	@Test
 	public void visit() {
 
