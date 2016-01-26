8c5580a21888a287ee5b6d654d9c218c72c3a00c
donghyuck <andang.son@gmail.com>
2016-01-26 12:12:36 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

---
+++ architecture-common/src/main/java/architecture/common/model/EntityModel.java
@@ -0,0 +1,15 @@
+package architecture.common.model;
+
+import java.io.Serializable;
+
+import architecture.common.cache.Cacheable;
+
+public interface EntityModel extends Cacheable {
+
+	public Serializable getPrimaryKeyObject();	
+	
+	public int getObjectType();
+	
+	public long getObjectId();
+		
+}
