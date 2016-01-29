eae5a119c670bf0dd26a63cf2f0d51dd6cd71795
donghyuck <andang.son@gmail.com>
2016-01-29 16:49:23 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

--- architecture-common/src/main/java/architecture/common/model/json/CustomJsonDateDeserializer.java
+++ architecture-common/src/main/java/architecture/common/model/json/CustomJsonDateDeserializer.java
@@ -17,20 +17,21 @@ package architecture.common.model.json;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 import architecture.common.exception.RuntimeError;
 
 public class CustomJsonDateDeserializer extends JsonDeserializer<Date> {
 	
-	private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-
+	//private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+	private static final ISO8601DateFormat formatter = new ISO8601DateFormat();
+	
 	@Override
 	public Date deserialize(JsonParser jsonparser,
 			DeserializationContext deserializationcontext) throws IOException,
@@ -39,7 +40,7 @@ public class CustomJsonDateDeserializer extends JsonDeserializer<Date> {
 		String date = jsonparser.getText();
 		
 		try {
-			return format.parse(date);
+			return formatter.parse(date);
 		} catch (ParseException e) {
 			throw new RuntimeError(e);
 		}
--- architecture-common/src/main/java/architecture/common/model/json/CustomJsonDateSerializer.java
+++ architecture-common/src/main/java/architecture/common/model/json/CustomJsonDateSerializer.java
@@ -16,20 +16,22 @@
 package architecture.common.model.json;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 public class CustomJsonDateSerializer extends JsonSerializer<Date> {
-
+	
+	private static final ISO8601DateFormat formatter = new ISO8601DateFormat();
+	
 	@Override
 	  public void serialize(Date value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-	    DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+	    //DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+		
 	    jgen.writeString(formatter.format(value));
 	  }
 
